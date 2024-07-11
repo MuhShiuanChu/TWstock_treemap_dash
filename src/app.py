1535074521746f55dbeaab3fd43f8f0b188414d0
MuhShiuanChu <43460629+MuhShiuanChu@users.noreply.github.com>
2024-07-11 11:43:02 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ app = Dash(__name__, title="Test")
 
 server = app.server
 
-data_file = 'test0701.csv'
+data_file = 'test0711.csv'
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
